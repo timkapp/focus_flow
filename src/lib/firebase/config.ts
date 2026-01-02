bca80d18ff3178426783468f2efeb486e63f7964
timkapp <timkapp@gmail.com>
2026-01-02 14:17:38 -0700

    Refactor: Use environment variables for Firebase config

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB1DyGTdHZGqZg4JJnqUpoi3HCkWRyjDA0",
-    authDomain: "focusflow-42119.firebaseapp.com",
-    projectId: "focusflow-42119",
-    storageBucket: "focusflow-42119.firebasestorage.app",
-    messagingSenderId: "266774563474",
-    appId: "1:266774563474:web:0aa6cba04a2ecd6955f44b"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
